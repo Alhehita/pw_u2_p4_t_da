f2fa6ab84f1131bf81f56d07d0e220c7c8546be2
Dennisse Andrade <daandradem1@uce.edu.ec>
2023-06-20 17:11:03 -0500

    Tarea 6

---
+++ src/main/java/com/example/demo/PwU2P4TDaApplication.java
@@ -0,0 +1,67 @@
+package com.example.demo;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.example.demo.repository.modelo.Autor;
+import com.example.demo.repository.modelo.Libro;
+import com.example.demo.service.AutorService;
+import com.example.demo.service.LibroService;
+
+@SpringBootApplication
+public class PwU2P4TDaApplicationimplements CommandLineRunner {
+
+	@Autowired
+	private LibroService libroService;
+
+	@Autowired
+	private AutorService autorService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U2P4AcDaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+
+		Libro libro = new Libro();
+		libro.setEditorial("La uce");
+		libro.setTitulo("La importancia de llamarse Jhon");
+		
+		Libro libro1 = new Libro();
+		libro1.setEditorial("La uce");
+		libro1.setTitulo("Mil y una noches");
+	
+		
+		Autor autor = new Autor();
+		autor.setNombre("Mauricio");
+		autor.setApellido("Cacuango");
+		
+		
+		Set<Autor> autores = new HashSet<>();
+		autores.add(autor);
+		
+		libro.setAutores(autores);
+
+		
+		Set<Libro> libros = new HashSet<>();
+		libros.add(libro);
+		libros.add(libro1);
+
+		autor.setLibros(libros);
+		
+		//this.libroService.agregar(libro);
+		autorService.agregar(autor);
+		
+		
+	
+	
+	}
+
+}
---
+++ src/main/java/com/example/demo/repository/modelo/Hotel.java
@@ -0,0 +1,67 @@
+package com.example.demo.repository.modelo;
+
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+@Entity
+@Table(name = "habitacion")
+public class Hotel {
+	@GeneratedValue(generator = "seq_habitacion", strategy = GenerationType.SEQUENCE)
+	@SequenceGenerator(name = "seq_habitacion", sequenceName = "seq_habitacion", allocationSize = 1)
+	@Id
+	@Column(name = "hote_id")
+	private Integer id;
+	
+	@Column(name = "hote_nombre")
+	private String nombre;
+	
+	@Column(name = "hote_direccion")
+	private String direccion;
+
+	@OneToMany(mappedBy = "hotel")
+	private List<Habitacion> habitaciones;
+
+	
+	//Set y Get
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public List<Habitacion> getHabitaciones() {
+		return habitaciones;
+	}
+
+	public void setHabitaciones(List<Habitacion> habitaciones) {
+		this.habitaciones = habitaciones;
+	}
+
+	
+	
+}
